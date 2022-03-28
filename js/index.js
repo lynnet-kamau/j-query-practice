38dd92ab1d8392be654cd2f2f0b5cfae594b6ec9
lynnet-kamau <linetwanjiku033@gmail.com>
2022-03-29 00:05:13 +0300

    fist commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+//     $("#hide").click(function() {
+//         $("p").animate({
+//             right: '400px'
+//         });
+//     });
+//     $(document).ready(function() {
+//         $("#show").click(function() {
+//             $("p").animate({
+//                 left: '250px',
+//                 height: '+=150px',
+//                 width: '+=150px',
+//             });
+//         });
+
+
+//     })
+
+// });
+$(document).ready(function() {
+            $("button").click(function() {
+                $("p").css({
+                    "background-color": "yellow",
+                    font - size 200 % " });
+                });
+            });
